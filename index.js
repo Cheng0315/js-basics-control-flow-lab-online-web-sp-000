ab52e602c42451a54b50c35080495e30df1802cd
Cheng Vue <csalvue@gmail.com>
2019-02-23 11:46:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,5 +2,9 @@
 function scuberGreetingForFeet(value) {
   if (value <= 400) {
     return 'This one is on me!'
+  } else if (value <= 2000) {
+    return 'I will gladly take your thirty bucks.'
+  } else if (value <= 2500) {
+    return 'No can do.'
   }
 }
